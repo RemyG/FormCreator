b2d21cb1666b6b52b3fdcc7bae783652058fc980
Remy Gardette <remy.gardette@gmail.com>
2012-03-08 11:54:27 +0000

    First version

---
+++ formCreator.js
@@ -0,0 +1,35 @@
+function parseFormStructure() {
+
+	var sForm = "&lt;form ";
+
+	var formAction = document.getElementById('form_action').value;
+
+	var formMethod = document.getElementById('form_method').value;
+
+	sForm += "action='"+formAction+"' ";
+	sForm += "method='"+formMethod+"' ";
+
+	sForm += "&gt;<br/>";
+
+	var formParameterRegex = document.getElementById('form_regex').value;
+
+	var formParameters = document.getElementById('form_parameters').value;
+
+	var patt1 = new RegExp(formParameterRegex, "g");
+
+	var result = patt1.exec(formParameters);
+
+	while(result != null) {
+
+		sForm += "&lt;input type='hidden' name='"+result[1]+"' value='"+result[2]+"' /&gt;<br/>";
+		result = patt1.exec(formParameters);
+
+	}
+
+	sForm += "&lt;input type='submit' /&gt;<br/>"
+
+	sForm += "&lt;/form&gt;"
+
+	document.getElementById('form_result').innerHTML = sForm;
+
+}
