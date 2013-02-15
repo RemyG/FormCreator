500a59cecd719225c6b1d1fa63b32df498764c2d
RemyG <remy.gardette@gmail.com>
2013-02-15 15:30:23 +0000

    Added responsive design, changed the license

--- formCreator.js
+++ formCreator.js
@@ -1,20 +1,11 @@
 /*
 	This file is part of FormCreator.
 
-	Copyright 2012 Remy Gardette
+	Copyright (c) 2012-2013 Remy Gardette
 
-	FormCreator is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Lesser General Public License as 
-	published by the Free Software Foundation, either version 3 of 
-	the License, or (at your option) any later version.
-
-	FormCreator is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License
-	along with FormCreator.  If not, see <http://www.gnu.org/licenses/>.
+	This software has been released under the MIT License. 
+	You should have received a copy of the MIT License along with this software. 
+	If not, see <>http://opensource.org/licenses/MIT>.
 */
 
 function parseFormStructure() {
